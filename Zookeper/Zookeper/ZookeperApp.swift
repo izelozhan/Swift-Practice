7e4f0223396d1b835cb9bd15d750e671ba49cc21
Izel Ozhan <izelozhan@gmail.com>
2025-08-18 14:30:06 -0500

    init

---
+++ Zookeper/Zookeper/ZookeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeperApp.swift
+//  Zookeper
+//
+//  Created by Izel on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ZookeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
