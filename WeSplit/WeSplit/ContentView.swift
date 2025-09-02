f2e1d0c4daa7b2bc1f2d7a9edff432207aac22dc
Izel Ozhan <izelozhan@gmail.com>
2025-09-02 15:10:36 -0500

    init

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Izel Ozhan on 9/2/25.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+                Section {
+                    Text("How much tip do you want to leave?")
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section {
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    //Since it's State, it auto check for changes/updates.
+                    //Locale is a massive struct built into iOS, responsible for storing all the user's region settings
+                }
+            }.navigationTitle("WeSplit").navigationBarTitleDisplayMode(.inline)
+        }
+        
+    }
+}
+
+//this is special piece of code that won't actually form part of your final app that goes to the app store.
+#Preview {
+    ContentView()
+}
