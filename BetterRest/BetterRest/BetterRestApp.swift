c7d686725bc78b66dfd4c95ec3f7c6ad4df5fbc5
Izel Ozhan <izelozhan@gmail.com>
2025-09-12 16:18:31 -0500

    initial

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Izel on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
